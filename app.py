62ba0ed70d7bd08f58b43c011ecaf28136a1ee3f
xsl <xsl@headmark>
2026-06-06 19:14:03 +0800

    save code

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ async def download_mask(type: str = "contour"):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8002, reload=True)
